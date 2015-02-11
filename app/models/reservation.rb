f0fdbf8bb23a4f058738f80491e7277785146e27
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-02-11 16:05:02 -0500

    Fixed typos in model associations, checked in console

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :restaurant
-	beongs_to :user
+	belongs_to :user
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ActiveRecord::Base
-	has_many :restaurants
+	has_many :reservations
 	has_many :users, through: :reservations
 end
