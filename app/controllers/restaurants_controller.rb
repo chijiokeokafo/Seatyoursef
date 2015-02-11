b00f27e2ae64080fc52858495d5c09ccbaa9ac77
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-02-11 17:38:16 -0500

    Fixed typos on new.html, tested adding a new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,7 +7,23 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-``end
+  	@restaurant = Restaurant.new
+  end
+  def create
+  	@restaurant = Restaurant.new(product_params)
+
+  	if @restaurant.save
+  		redirect_to restaurants_url
+  	else
+  		render :new
+  	end	
+  end
+
+  private
+  def product_params
+  	params.require(:restaurant).permit(:name, :open_time, :close_time, :capacity)
+  end 	
+  
 
  
 end
