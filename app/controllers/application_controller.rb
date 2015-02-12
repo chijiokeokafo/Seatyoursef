66ceff5ff7ac1b650d7f2c584f5dbf7ab2b77301
Brant <boshartb@gmail.com>
2015-02-12 18:54:45 -0500

    Bug fixed on login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :insure_login
-  protect_from_forgery with: :exception
+  # before_filter :insure_login
+  # before_filter :insure_login, except: [:index, :show, :]
 
-    private
+
+  private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
