f5d65a82c9aeb704070e7d330e32a8abc34fc644
Brant <boshartb@gmail.com>
2015-02-12 16:03:29 -0500

    edit and destroy funtions added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,8 +13,25 @@ class RestaurantsController < ApplicationController
       # @reservation = @restaurant.reservations.build
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if current_user == @restaurant.user
+      if @restaurant.update_attributes(restaurant_params)
+        redirect_to restaurant_path(@restaurant)
+      else
+        render :edit
+      end
+    end
+  end
+
+
   def create
   	@restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user == current_user
 
   	if @restaurant.save
   		redirect_to restaurants_url
@@ -23,11 +40,17 @@ class RestaurantsController < ApplicationController
   	end	
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    if current_user == @restaurant.user
+         @restaurant.destroy
+         redirect_to restaurants_path
+      end
+  end
+
   private
   def restaurant_params
   	params.require(:restaurant).permit(:name, :open_time, :close_time, :capacity)
   end 	
-  
-
- 
 end
+
