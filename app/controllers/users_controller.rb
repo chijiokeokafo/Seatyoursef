ee167b11d169ef17758e0b2685a59b2edca583f6
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-02-12 13:11:33 -0500

    Adding has_secure_password and bcrypt file to project, added new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,19 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-	def new
-		@user = User.new
-	end
-
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			redirect_to restaurants_url, notice: "Signed up"
-		else
-			render "new"
-		end
-	end
-
-	private
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-end
-=======
   def new
-  	
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to restaurants_url, notice: "Signed up!"
+    else
+      render "new"
+    end
   end
->>>>>>> 7009a47373d59190d5a149f09e6b64285ea56aff
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # get 'users/create'
   resources :restaurants
   resources :reservations
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
