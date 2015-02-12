c54dd9654912ba8394379ed3d59e36a74f51b208
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-02-12 11:00:57 -0500

    Added validations to restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ActiveRecord::Base
+	
+  	validates :name, presence: true
+  	validates :open_time, :close_time, :capacity, numericality: {only_integer: true}
+
 	has_many :reservations
 	has_many :users, through: :reservations
 end
+
