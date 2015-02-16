8638ada386d0eda247a9d3f73a2b19e837996b06
Brant <boshartb@gmail.com>
2015-02-16 10:44:17 -0500

    added phone number to resto table/form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216153013) do
+ActiveRecord::Schema.define(version: 20150216153958) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "party_size"
@@ -28,10 +28,11 @@ ActiveRecord::Schema.define(version: 20150216153013) do
     t.integer  "open_time"
     t.integer  "close_time"
     t.integer  "capacity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.string   "address"
+    t.string   "phone_number"
   end
 
   create_table "users", force: :cascade do |t|
