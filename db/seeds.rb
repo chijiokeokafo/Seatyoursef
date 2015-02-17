cb9b445a625d1b56dc4a2226d54f5a4792728da1
mkanamoto <mkanamoto@gmail.com>
2015-02-17 16:05:19 -0500

    added seeds and remigrate file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.create!(
+	first_name: "Bit",
+	last_name: "Maker",
+	email: "bitmaker@bitmaker.com",
+	password: "hello",
+	password_confirmation: "hello"
+)
+
+Restaurant.create!(
+	name: "Stan's Slop Shack",
+	open_time: 9,
+	close_time: 22,
+	capacity: 51,
+	user: u,
+	address: "57 What St.",
+	phone_number: "555-55-55555-55-555",
+	description: "Best slop in town"
+)
+
+Restaurant.create!(
+	name: "Hello Restaurant",
+	open_time: 4,
+	close_time: 10,
+	capacity: 100,
+	user: u,
+	address: "55 hello street",
+	phone_number: "555-888-8888",
+	description: "Yummy"
+)
+
+Restaurant.create!(
+	name: "Open",
+	open_time: 4,
+	close_time: 10,
+	capacity: 50,
+	user: u,
+	address: "100 Bathurst Street",
+	phone_number: "555-888-8888",
+	description: "Always open"
+)
